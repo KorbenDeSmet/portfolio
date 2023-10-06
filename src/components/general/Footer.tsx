d304dd8e8eea1dde5937babd3bd13ad819329dad
KorbenDeSmet <korbendesmet@hotmail.com>
2023-10-06 22:42:44 +0200

    migrated to vite

--- src/components/general/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <div className="bg-primaryLight dark:bg-primaryDark text-primaryDark dark:text-primaryLight h-96">
-            Footer
-        </div>
-    )
-};
