048acc7458720dbe2ac3a12f039dc3fd9d677e0d
KorbenDeSmet <korbendesmet@hotmail.com>
2023-10-07 23:32:07 +0200

    Created Experience section, custom scrollbar and added temp navbar

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,11 +5,11 @@ import GeneralInformation from './../sections/GeneralInformation'
 
 const App = () => {
   return (
-    <div className="flex min-h-screen bg-neutral-100">
+    <div className="flex min-h-screen pb-24">
       <div className="w-[42rem] mx-auto px-16">
         <h1 className="font-serif mt-28 text-3xl">Korben De Smet</h1>
         <h5 className="text-xs">Web & Mobile Developer</h5>
-        <p className="text-xs mt-12 text-center">A highly motivated developer who graduated from the College of Ghent with
+        <p className="text-xs mt-12 text-center leading-5">A highly motivated developer who graduated from the College of Ghent with
           a degree in Mobile & Enterprise Development. People often describe me as
           flexible and very friendly. A characteristic of mine is that I always view
           situations from multiple points of view. I am highly proficient in working in
@@ -18,10 +18,28 @@ const App = () => {
           I also work out at the gym.</p>
 
         <Experience />
-        {/* <GeneralInformation />
-        <Education />
-        <Awards /> */}
-
+        {/* <GeneralInformation /> */}
+        {/* <Education /> */}
+        {/* <Awards /> */}
+      </div>
+      <div className="fixed bottom-0 w-full">
+        <div className="flex gap-8 w-fit mb-4 mx-auto p-2 rounded-full shadow-2xl bg-neutral-800 text-white">
+          <button className="bg-neutral-100 p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#262626" className="bi bi-house-door-fill" viewBox="0 0 16 16">
+              <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
+            </svg>
+          </button>
+          <button className="p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+            </svg>
+          </button>
+          <button className="p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-balloon-fill" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8.48 10.901C11.211 10.227 13 7.837 13 5A5 5 0 0 0 3 5c0 2.837 1.789 5.227 4.52 5.901l-.244.487a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.244-.487ZM4.352 3.356a4.004 4.004 0 0 1 3.15-2.325C7.774.997 8 1.224 8 1.5c0 .276-.226.496-.498.542-.95.162-1.749.78-2.173 1.617a.595.595 0 0 1-.52.341c-.346 0-.599-.329-.457-.644Z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div >
   )
--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,45 +1,65 @@
+import { useState } from "react"
+import { Collapse } from 'react-collapse';
+
 const Experience = () => {
+    const [isCodiflyVisible, setIsCodiflyVisible] = useState(false);
+    const [isHoGentVisible, setIsHoGentVisible] = useState(false);
+
     return (
-        <div className="mt-12 text-xs">
-            <h3 className="mb-4 text-base">Experience</h3>
-            <div className="grid grid-cols-5 gap-x-2 gap-y-6">
-                <div className="shrink">February 2023 - May 2023</div>
+        <div className="mt-12">
+            <h3>Experience</h3>
+            <div className="grid grid-cols-5 gap-x-2 gap-y-6 mt-4">
+                <div className="pt-1 text-sm">October 2023 - Present</div>
                 <div className="col-span-4">
-                    <div>Web & Mobile developer intern @ Codifly</div>
-                    <div>Codifly is a consultancy firm that has a focus on web, mobile and IoT applications.</div>
-                    {/* Possibly hide technologies on click? */}
-                    <h5 className="mt-4">Technologies used during my employment:</h5>
-                    <p>React Native, React, TypeScript, Apollo Client, Apollo Server, Node.js, GraphQL, Styled Components, Docker, PostgreSQL, Fastlane, bash, AWS, S3</p>
+                    <div>Software developer @ Endare</div>
+                    <div className="inline-block text-sm text-neutral-600 no-underline">TODO</div>
                 </div>
-                <div>2019 - 2023</div>
+                <div className="pt-1 text-sm">February 2023 - May 2023</div>
                 <div className="col-span-4">
-                    <div>Student @ HoGent</div>
-                    <div>During my degree at HoGent, I successfully completed multiple
-                        projects. These were carried out in task of different companies such as
-                        HoySpain Renting, Squads and Quarante. For my bachelor’s thesis I
-                        collaborated with Bryan Brouckaert from Egelke Bv to research the
-                        following topic: “A detailed performance comparison between the
-                        different NoSQL database types when processing personal data”.</div>
-                    <h5 className="mt-4">Technologies used during my employment:</h5>
-                    <p>React, Tailwind css, JavaScript, Cypress, postcss, axios, daisyui,
-                        heroku, NodeJs, Koa, Knex, Github Pages, .net, Blazor, C#, Domain
-                        driven design, EntityFrameworkCore, AspNetCore, Data transfer
-                        objects, micro services, Kotlin, gradle, nuget, express, MongoDb,
-                        Cassandra, redis, Neo4J, cypher, MQL, CQL</p>
+                    <button className="text-left hover:underline mb-4" onClick={() => setIsCodiflyVisible(!isCodiflyVisible)}>
+                        <div>Web & Mobile developer intern @ Codifly</div>
+                        <div className="inline-block text-sm text-neutral-600 no-underline">Codifly is a consultancy firm that has a focus on web, mobile and IoT applications.</div>
+                    </button>
+                    <Collapse isOpened={isCodiflyVisible}>
+                        <h5 className="text-sm text-neutral-600">Technologies used during my employement:</h5>
+                        <p className="text-sm text-neutral-600">React Native, React, TypeScript, Apollo Client, Apollo Server, Node.js, GraphQL, Styled Components, Docker, PostgreSQL, Fastlane, bash, AWS, S3</p>
+                    </Collapse>
                 </div>
-                <div className="shrink">January 2019 - February 2019</div>
+                <div className="pt-1 text-sm">2019 - 2023</div>
+                <div className="col-span-4">
+                    <button className="text-left hover:underline mb-4" onClick={() => setIsHoGentVisible(!isHoGentVisible)}>
+                        <div>Student @ HoGent</div>
+                        <div className="inline-block text-sm text-neutral-600 no-underline">During my degree at HoGent, I successfully completed multiple
+                            projects. These were carried out in task of different companies such as
+                            HoySpain Renting, Squads and Quarante. For my bachelor's thesis I
+                            collaborated with Bryan Brouckaert from Egelke Bv to research the
+                            following topic: “A detailed performance comparison between the
+                            different NoSQL database types when processing personal data”.</div>
+                    </button>
+                    <Collapse isOpened={isHoGentVisible}>
+                        <h5 className="text-sm text-neutral-600">Technologies used during my employement:</h5>
+                        <div className="text-sm text-neutral-600">
+                            React, Tailwind css, JavaScript, Cypress, postcss, axios, daisyui,
+                            heroku, NodeJs, Koa, Knex, Github Pages, .net, Blazor, C#, Domain
+                            driven design, EntityFrameworkCore, AspNetCore, Data transfer
+                            objects, micro services, Kotlin, gradle, nuget, express, MongoDb,
+                            Cassandra, redis, Neo4J, cypher, MQL, CQL
+                        </div>
+                    </Collapse>
+                </div>
+                <div className="pt-1 text-sm">January 2019 - February 2019</div>
                 <div className="col-span-4">
                     <div>IT Support intern @ OLV Hospital Alost</div>
-                    <div>At the OLV Hospital I helped support the technical infrastructure and assisted doctors when problems occurred.</div>
+                    <div className="text-sm text-neutral-600">At the OLV Hospital I helped support the technical infrastructure and assisted doctors when problems occurred.</div>
                 </div>
-                <div className="shrink">2017 - 2020</div>
+                <div className="pt-1 text-sm">2017 - 2020</div>
                 <div className="col-span-4">
                     <div>IT Support @ Via Sophia</div>
-                    <div>Via Sophia is a boutique with two locations. I built their webshop using
+                    <div className="text-sm text-neutral-600">Via Sophia is a boutique with two locations. I built their webshop using
                         Prestashop and offer tech support during the weekends. Additionally, I
                         was also responsible for designing their logos and flyers.</div>
                 </div>
-            </div>
+            </div >
         </div >
     )
 }
